182b38373bc24258f9c7d199ee779323625d8ecd
Mostafa Mansour <73035890+Mostafa-Y-Mansour@users.noreply.github.com>
2023-12-20 22:05:53 +0200

    first commit app initialization

---
+++ src/components/Alert.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+interface IProps {
+  title: string;
+  description: string;
+}
+
+const Alert: FC<IProps> = ({ title, description }) => {
+  return <></>;
+};
+
+export default Alert;
